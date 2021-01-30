7714bf8406523e3247c011fa053a1c235952fcd5
Steve Lambert <steve.lambert67@gmail.com>
2021-01-30 15:32:50 +0000

    passport template working

--- passport/index.js
+++ passport/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const session = require('express-session');
 const mongoose = require('mongoose');
+const session = require('express-session');
+var passport = require('passport');
+var crypto = require('crypto');  // allows for creation and verification of passwords
 var routes = require('./routes');
+const connection = require('./config/database');
 
+// Package documentation - https://www.npmjs.com/package/connect-mongo
+const MongoStore = require('connect-mongo')(session);
 
 /**
  * -------------- GENERAL SETUP ----------------
@@ -16,6 +21,33 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
+/**
+ * -------------- SESSION SETUP ----------------
+ */
+
+const sessionStore = new MongoStore({ mongooseConnection: connection, collection: 'sessions' });
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    }
+}));
+
+/**
+ * -------------- PASSPORT AUTHENTICATION ----------------
+ */
+
+ // Need to require the entire Passport config module so app.js knows about it
+require('./config/passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 /**
