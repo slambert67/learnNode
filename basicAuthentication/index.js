6c2f2110b58cbd5894b9c8c72eb809a11b34645f
Steve Lambert <steve.lambert67@gmail.com>
2021-02-03 20:13:06 +0000

    basic authentication

---
+++ basicAuthentication/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+// create the express application
+const app = express();
+
+
+app.get('/', (req, res, next) => {
+    res.status_code = 200;
+    res.end("Welcome to your express app!");
+});
+
+// define a route needing authentication
+app.get('/secret', (req, res, next) => {
+
+    /**
+     * if credentials have been passed they'll be in a header called Authorization
+     * (note NodeJS lowercases the names of headers in its request object)
+     */
+
+    // var auth = req.headers['authorization'];
+    var auth = req.headers.authorization;
+    console.log('auth initialized as - ' + auth);
+    /**
+     * initial request will likely have no credentials so auth will be undefined
+     */
+    if (!auth) {   
+        console.log('credentials not supplied');
+        // No Authorization header was passed in so it's the first time the browser hit us
+
+        // Sending a 401 will require authentication, we need to send the 'WWW-Authenticate' to tell them the sort of authentication to use
+        // Basic auth is quite literally the easiest and least secure, it simply gives back  base64( username + ":" + password ) from the browser
+        // 401 ensures browser prompts for username/password
+        res.statusCode = 401;  
+        res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+
+        res.end('<html><body>Need some credentials</body></html>');
+    } 
+    else {
+        console.log('we have credentials - ' + auth);
+
+        var tmp = auth.split(' ');   // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
+
+        // convert from base64
+        var buf = new Buffer.from(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
+        var plain_auth = buf.toString();             // read it back out as a string
+
+        console.log("Decoded Authorization ", plain_auth);
+
+        // At this point plain_auth = "username:password"
+
+        var creds = plain_auth.split(':');      // split on a ':'
+        var username = creds[0];
+        var password = creds[1];
+
+        if((username == 'squoink') && (password == 'oldpeculier')) {   // Is the username/password correct? Check entries in database here!
+
+            res.statusCode = 200;  // OK
+            res.end('<html><body>Congratulations - credentials accepted!</body></html>');
+        }
+        else {
+            res.statusCode = 401; // Force them to retry authentication
+            res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+
+            // res.statusCode = 403;   // or alternatively just reject them altogether with a 403 Forbidden
+
+            res.end('<html><body>You shall not pass</body></html>');
+        }
+    }
+});
+
+
+
+
+app.listen(3000);
--- passport/config/passport.js
+++ passport/config/passport.js
@@ -3,4 +3,49 @@ const LocalStrategy = require('passport-local').Strategy;
 const connection = require('./database');
 const User = connection.models.User;
 
+
+// overrides default local strategy and specifies where to get values for varifyCallback
+const customFields = {
+    usernameField: 'uname',
+    passwordField: 'pw'
+};
+
+
+// our passport strategy needs a verify callback
+const verifyCallback = (username, password, done) => {
+    // done is a function that results of authentication is passed to
+    // post request will have username/password from html form completed by user
+    // parameters populated by passport framework
+    // so passport local-strategy needs naming to be correct
+    // can override and tell passport which filds to look in
+    // essentially we're writing our own implementation of password verification
+    // which database doesn't matter
+    // how we verify credentials doesn't matter
+    // all that matters is that parameters passed to 'done' are what passport expects
+
+    // go to mongo database
+    User.findOne({ username: username })
+    .then( (user) => {
+        // user populated by result of findOne
+
+        // if not found. null=>no error, false=>no user found
+        if (!user) { return done(null, false) }
+
+        const isValid = validPassword(password, user.hash, user.salt);  // not yet implemented
+
+        if (isValid) {
+            return done(null,user);
+        } else {
+            return done(null, false);
+        }
+    })
+    .catch( (err) => {
+        done(err);
+    });
+}
+
+// first define what passport strategy we are going to use
+const strategy = new LocalStrategy();
+
+
 // TODO: passport.use();
